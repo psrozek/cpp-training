a76f625c53b66e1645f583e80cacd82a6f325a56
Piotr <82279077+psrozek@users.noreply.github.com>
2021-07-30 10:06:59 +0200

    Create basic_function.cpp

---
+++ basic_function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int Max(int a, int b)
+{
+	if (a > b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+int main()
+{
+	int value1 = 0;
+	int value2 = 0;
+
+	std::cout << "Please input number 1: ";
+	std::cin >> value1;
+
+	std::cout << "Please input number 2: ";
+	std::cin >> value2;
+
+	std::cout << "The higher number is " << Max(value1, value2) << ".";
+}
