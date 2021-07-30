eea875b259d1571cddb374c6e9b9324a023763a5
Piotr <82279077+psrozek@users.noreply.github.com>
2021-07-30 10:21:03 +0200

    Update ticket_machine.cpp

--- ticket_machine.cpp
+++ ticket_machine.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <string>
 
+bool bIsRunning = true;
 int main()
 {
-	std::string input;
-	int number;
-
-	std::cout << "Ticket machine:\n";
-	std::cout << "1. 30-minutes ticket\n";
-	std::cout << "2. 1-hour ticket\n";
-	std::cout << "3. 1-day ticket\n";
-	std::cout << "Please enter a number 1-3 to view a ticket price: ";
-	getline(std::cin, input);
-	number = std::stoi(input);
-
-	switch (number)
+	while (bIsRunning)
 	{
-	case 1:
-		std::cout << "30-minutes ticket: $0.99\n";
-		break;
+		std::string input;
+		int number;
 
-	case 2:
-		std::cout << "1-hour ticket: $1.99\n";
-		break;
+		std::cout << "Ticket machine:\n";
+		std::cout << "1. 30-minutes ticket\n";
+		std::cout << "2. 1-hour ticket\n";
+		std::cout << "3. 1-day ticket\n";
+		std::cout << "Please enter a number 1-3 to view a ticket price: ";
+		getline(std::cin, input);
+		number = std::stoi(input);
 
-	case 3:
-		std::cout << "1-day ticket: $4.99\n";
-		break;
+		switch (number)
+		{
+		case 1:
+			std::cout << "30-minutes ticket: $0.99\n";
+			break;
 
-	default:
-		std::cout << "Invalid choice.\n";
-		break;
+		case 2:
+			std::cout << "1-hour ticket: $1.99\n";
+			break;
+
+		case 3:
+			std::cout << "1-day ticket: $4.99\n";
+			break;
+
+		default:
+			std::cout << "Invalid choice.\n";
+			break;
+		}
 	}
 }
