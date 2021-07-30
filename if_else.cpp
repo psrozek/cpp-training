05e60246ee7975ee80f402878de1e6aca3b80d40
Piotr <82279077+psrozek@users.noreply.github.com>
2021-07-30 10:12:00 +0200

    Create if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string input;
+	int number;
+
+	std::cout << "Please enter a number: ";
+	getline(std::cin, input);
+	number = std::stoi(input);
+
+	if (number < 10)
+	{
+		std::cout << "The number you entered was less than 10!\n";
+	}
+	else if (number > 10)
+	{
+		std::cout << "The number you entered was greater than 10!\n";
+	}
+	else
+	{
+		std::cout << "The number you entered was equal to 10!\n";
+	}
+	return 0;
+}
